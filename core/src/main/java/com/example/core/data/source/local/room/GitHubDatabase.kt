3a84943d780544e6749121b221736c66702c20fd
ramsi <dhiyaulhaqahmadramsi@gmail.com>
2024-05-09 18:32:30 +0700

    change database schema

--- core/src/main/java/com/example/core/data/source/local/room/GitHubDatabase.kt
+++ core/src/main/java/com/example/core/data/source/local/room/GitHubDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.core.data.source.local.entity.FavoriteUserEntity
 
-@Database(entities = [FavoriteUserEntity::class], version = 1, exportSchema = true)
+@Database(entities = [FavoriteUserEntity::class], version = 1, exportSchema = false)
 abstract class GitHubDatabase : RoomDatabase() {
 
     abstract fun gitHubDao(): GitHubDao
